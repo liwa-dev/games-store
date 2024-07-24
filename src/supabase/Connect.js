8371de417a0450b6c5951cee528dcef428d6e822
Liwa Hadri <66137680+liwa-dev@users.noreply.github.com>
2024-07-24 15:06:10 +0100

    Update Connect.js
    REMOVE KEYS API

--- src/supabase/Connect.js
+++ src/supabase/Connect.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rtjvfpkuyiwvywklhsbc.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0anZmcGt1eWl3dnl3a2xoc2JjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDg2NzAxMywiZXhwIjoyMDM2NDQzMDEzfQ.adSMdNP0h1MTu_LlX6eh-96BlsuQ4aSHV5J2bzklTXY';
+const supabaseUrl = 'youre url here';
+const supabaseKey = 'youre key here';
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
